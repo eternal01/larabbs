46ca78e841c7ccb9f4ee4d00cd2a176fb9aa658f
muxiaokang <soulmask.mr@gmail.com>
2020-01-21 16:14:01 +0800

    修复 N+1 问题

---
+++ app/Http/Controllers/TopicsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Topic;
+
+class TopicsController extends Controller
+{
+    public function index()
+    {
+        $topics = Topic::with('user', 'category')->paginate(30);
+        return view('topics.index', compact('topics'));
+    }
+}
