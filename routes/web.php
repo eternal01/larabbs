c358c998c6c2e8f5ddf97832960cf7418ad4b0ec
muxiaokang <soulmask.mr@gmail.com>
2020-03-20 16:17:58 +0800

    用户可以删除自己发的话题

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->na
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'show', 'update', 'edit']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'show', 'update', 'edit', 'destroy']]);
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
