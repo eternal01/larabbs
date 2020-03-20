22e6406e6ed1fffaa793f10d1a03cfc89fecfefe
muxiaokang <soulmask.mr@gmail.com>
2020-03-20 16:06:29 +0800

    用户可以编辑话题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -42,6 +42,21 @@ class TopicsController extends Controller
         return view('topics.show',compact('topic'));
     }
 
+    public function edit(Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        $categories = Category::all();
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+    }
+
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
+
+        return redirect()->route('topics.show', $topic->id)->with('success', '更新成功！');
+    }
+
     public function uploadImage(Request $request,ImageUploadHandler $uploader)
     {
         // 初始化返回数据，默认是失败的
