d9996ce522f0309388191b377d2f4f6e26c0deaa
muxiaokang <soulmask.mr@gmail.com>
2020-04-13 16:57:54 +0800

    回复数据

---
+++ database/migrations/2020_04_13_165251_create_replies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('topic_id')->index()->unsigned()->default(0)->comment('帖子ID');
+            $table->bigInteger('user_id')->index()->unsigned()->comment('用户ID');
+            $table->text('content')->nullable()->comment('回复内容');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
